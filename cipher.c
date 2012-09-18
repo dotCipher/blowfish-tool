44fa4873c42c88d9647dc4ace86f9d74bbc54a0c
Cody J Moore <cjmoore@a-centos56.cs.stonybrook.edu>
2012-09-18 13:40:39 -0400

    Added error checking for infile and outfile Including error checking for stdin and stdout

--- cipher.c
+++ cipher.c
@@ -1,8 +1,17 @@
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "blowfish.h"
+
+int fileExists(char *fName){
+  struct stat buffer;
+  return (stat(fName, &buffer)==0);
+}
 
 int main(int argc, char *argv[]){
   int DEBUG = 1;
@@ -12,9 +21,12 @@ int main(int argc, char *argv[]){
   /* Temp buffer to store user input (user password) */
   char temp_buf[16];
   //char temp_buf_chk[16];
-  char *version = "$Revision: 1.5 $";
+  char *version = "$Revision: 1.6 $";
+  /* File names/descriptors/stats*/
   char *infile_name;
   char *outfile_name;
+  int stdin_infile;
+  int stdout_outfile;
   int len;
 
   /* define boolean ints for params */
@@ -24,6 +36,7 @@ int main(int argc, char *argv[]){
 
   /* initialize and check params */
   len = 128;
+  stdin_infile = 0; stdout_outfile = 0;
   deco = 0; enco = 0; vers = 0; help = 0; mmap = 0; pass = 0; safe = 0;
   while((opt = getopt(argc, argv, "devhmsp:")) != -1) {
     switch(opt){
@@ -55,6 +68,7 @@ int main(int argc, char *argv[]){
     }
   }
   
+  // DEBUGGING CODE //
   if(DEBUG==1){
     printf("\ndecode=%i \nencode=%i \nversion=%i \nhelp=%i \nmmap=%i \npass=%i \n", deco, enco, vers, help, mmap, pass);
 
@@ -63,8 +77,10 @@ int main(int argc, char *argv[]){
     } else {
       printf("\ntemp_buf=<BLANK>\n");
     }
+    printf("argc=%i\n",argc);
   }
-
+  ////////////////////
+  
   if(help==1){
     printf("Blowfish Cipher Tool by Cody Moore \n");
     printf("Usage: %s [OPTIONS] [-p PASSWORD] <infile> <outfile> \n", argv[0]);
@@ -76,18 +92,42 @@ int main(int argc, char *argv[]){
     printf("   -h        :  Show help screen (you are looking at it) \n");
     printf("   -m        :  Enable memory mapping - mmap() \n");
     printf("   -s        :  Safe Mode (prompt for password twice\n");
+    return 0;
   } else if(vers==1){
     printf("Blowfish Cipher Tool - %s\n", version);
-  } else{
+    return 0;
+  } else if(argc>=3){
     // Take <infile> and <outfile>
     infile_name=(char*)malloc(sizeof(char)*strlen(argv[argc-2]));
     outfile_name=(char*)malloc(sizeof(char)*strlen(argv[argc-1]));
     strcpy(outfile_name, argv[argc-1]);
     strcpy(infile_name, argv[argc-2]);
+    
+    // Check if stdout or stdin is used
+    //  in replace of <infile> or <outfile>
+    if(strcmp(infile_name,"-")==0){
+      stdin_infile=1;
+    }
+    if(strcmp(outfile_name,"-")==0){
+      stdout_outfile=1;
+    }
+    
+    if(stdin_infile!=1){
+      if((fileExists(infile_name))!=1){
+        fprintf(stderr,"Error: Input file does not exist\n");
+        return 0;
+      }
+    }
+    
+    // DEBUGGING CODE //
     if(DEBUG==1){
       printf("infile_name=%s\n",infile_name);
       printf("outfile_name=%s\n",outfile_name);
+      printf("read from stdin=%i\n",stdin_infile);
+      printf("print to stdout=%i\n",stdout_outfile);
     }
+    ////////////////////
+    
     // Decryption Mode
     if(deco==1 && enco==0){
       // Password in cmd arg
@@ -96,9 +136,11 @@ int main(int argc, char *argv[]){
         
       } else{
         if(safe==1){
-      
+          // Ask for password twice
+          
         } else{
-      
+          // Ask for password once
+          
         }
       }
     // Encryption Mode
@@ -108,14 +150,18 @@ int main(int argc, char *argv[]){
         // temp_buf has password
       } else{
         if(safe==1){
-
+          // Ask for password twice
+          
         } else{
-
+          // Ask for password once
+          
         }
       }
     // Both Encrypt/Decrypt OR Neither Encrypt/Decrypt
     } else {
       fprintf(stderr, "Error: Must use EITHER Encrypt (-e) OR Decrypt (-d)\n");
+      fprintf(stderr, "       i.e.  %s [-e|-d] <infile> <outfile>\n",argv[0]);
+      return 0;
     }	
 
     /* don't worry about these two: just define/use them */
@@ -138,6 +184,10 @@ int main(int argc, char *argv[]){
     /* Decrypting is the same: just pass BF_DECRYPT instead */
     // BF_cfb64_encrypt(from, to, len, &key, iv, &n, BF_DECRYPT);
   
+  } else{
+    fprintf(stderr,"Error: Must include <infile> and <outfile> parameters\n");
+    fprintf(stderr,"       i.e.  %s [-e|-d] <infile> <outfile>\n", argv[0]);
+    return 0;
   }
   return 0;
 }
